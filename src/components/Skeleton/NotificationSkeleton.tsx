910c41f7bf775c756a221e2ae58394993869f6d0
Techeniac <techeniac-rishi@Techeniacs-Mac-mini.local>
2023-10-17 13:16:21 +0530

    bug fix : hydration error in save post

--- src/components/Skeleton/NotificationSkeleton.tsx
+++ src/components/Skeleton/NotificationSkeleton.tsx
@@ -3,21 +3,21 @@ import React from 'react';
 const NotificationSkeleton = () => {
   return (
     <div>
-      {Array.from({ length: 4 }).map((_, index) => (
+      {Array.from({ length: 5 }).map((_, index) => (
         <div className="bg-[#1f1f1f] rounded-[10px] p-4 shadow-md mb-4" key={index}>
           <div className="flex md:items-center cursor-pointer">
             <div className="mt-2 mr-2">
-            <div className="flex flex-row gap-3 w-full items-center mb-3">
-        <div className="skeleton-avatar bg-[#3f3f3f] animate-pulse"></div>
-        <div className="flex flex-col gap-1">
-          <div className="flex flex-row ml-0 gap-3 items-center">
-            <div className="skeleton-username-notification bg-[#3f3f3f] animate-pulse"></div>
-          </div>
-          <div className="flex flex-row justify-between mr-12 items-center mt-1">
-            <div className="skeleton-stats-notification bg-[#3f3f3f] animate-pulse"></div>
-          </div>
-        </div>
-      </div>
+              <div className="flex flex-row gap-3 w-full items-center mb-3">
+                <div className="skeleton-avatar bg-[#3f3f3f] animate-pulse"></div>
+                <div className="flex flex-col gap-1">
+                  <div className="flex flex-row ml-0 gap-3 items-center">
+                    <div className="skeleton-username-notification bg-[#3f3f3f] animate-pulse"></div>
+                  </div>
+                  <div className="flex flex-row justify-between mr-12 items-center mt-1">
+                    <div className="skeleton-stats-notification bg-[#3f3f3f] animate-pulse"></div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div>
               <div className="skeleton-text text-lg font-semibold" />
--- src/components/Skeleton/SavedBlogsSkeleton.tsx
+++ src/components/Skeleton/SavedBlogsSkeleton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SavedBlogsSkeleton = () => {
   return (
-    <>
+  <>
       {Array.from({ length: 6 }).map((_, index) => (
         <div
           className="blog-bg-ct p-5 rounded-2xl mt-16 w-full col-span-12 md:col-span-6 xl:col-span-4 h-auto cursor-pointer"
@@ -13,20 +13,20 @@ const SavedBlogsSkeleton = () => {
             <h3 className="text-white font-bold sm:text-[24px] text-[14px] cursor-pointer">
               <div className="skeleton-text w-4/5 h-6 bg-[#1f1f1f] rounded" />
             </h3>
-            <p className="mt-2 text-secondary sm:text-[18px] text-[11px]">
+            <div className="mt-2 text-secondary sm:text-[18px] text-[11px]">
               <div className="skeleton-text w-full h-4 bg-[#1f1f1f] rounded" />
               <div className="skeleton-text w-full h-4 mt-2 bg-[#1f1f1f] rounded" />
               <div className="skeleton-text w-4/5 h-4 mt-2 bg-[#1f1f1f] rounded" />
-            </p>
+            </div>
           </div>
           <div className="mt-4 flex-wrap gap-2 sm:flex hidden text-white">
             <div className="skeleton-text w-1/5 h-4 bg-[#1f1f1f] rounded" />
             <div className="skeleton-text w-1/5 h-4 bg-[#1f1f1f] rounded" />
-            <div className="skeleton-text w-1/5 h-4 bg-[#1f1f1f] rounded" />
+            <div className="skeleton-text w-1/5 h-4 bg-[#1f1f1f] rounded" />  
           </div>
         </div>
       ))}
-    </>
+  </>
   );
 };
 
